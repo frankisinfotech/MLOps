a814462301f4fa1290ddd02eb8ad6f8a0d757655
frankisinfotech <67714070+frankisinfotech@users.noreply.github.com>
2025-04-25 03:40:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+import numpy as np
+import pickle
+import json
 
+with open('model/model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+predictions = model.predict(np.array([[1,500]]))
+print("Prediced Rental Price: ", predictions[0])
