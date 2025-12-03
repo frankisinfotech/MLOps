fc3c7b0a51428661dd47635a1ba6a1fd954ca3c8
frankisinfotech <67714070+frankisinfotech@users.noreply.github.com>
2025-12-03 16:31:40 +0100

    Refactor model-app.py to remove pickling
    Removed model saving code and added prediction example.

---
+++ v1_code/model-app.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import root_mean_squared_error
+
+rentalDF = pd.read_csv('data/rental_1000.csv')
+
+# Features - X [ rooms && area ]
+# Label    - y [ price]
+X = rentalDF[['rooms','area']].values
+y = rentalDF['price'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+lr = LinearRegression()
+model = lr.fit(X_train,y_train)
+
+predictions = model.predict(np.array([2,611]))
+print("The predicted rental price is:", predictions[0])
